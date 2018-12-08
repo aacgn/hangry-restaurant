48b4841a63f40daf2df9b081dd4e533433481853
aacgn <4augusto@gmail.com>
2018-12-08 11:19:37 -0300

    Add axios

--- src/components/activities/Home.js
+++ src/components/activities/Home.js
@@ -14,6 +14,18 @@ export default class Home extends Component {
     }
   }
 
+  componentDidMount() {
+    axios.get('https://hangry-api.herokuapp.com/restaurant/5c08592af86b994f6fa2df0c')
+         .then((data) => {
+            console.log(data);
+            this.props.setStore({
+              data: {
+                restaurant: data.data
+              }
+            })
+    });
+  }
+
   render() {
     return (
       <div className="activity home">
@@ -23,7 +35,7 @@ export default class Home extends Component {
             <img className="activity__header-logo" src={Logo} />
             </HeaderItem>
             <HeaderItem>
-              <div className="activity__header-user-name">Hangryrestaurant</div>
+              <div className="activity__header-user-name">{"Hangryrestaurant"}</div>
               <img className="activity__header-user-image" src={User} />
             </HeaderItem>
           </div>
