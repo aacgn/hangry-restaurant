91c026b5350fbc431e0dff301121bb318172c056
aacgn <4augusto@gmail.com>
2018-12-08 01:31:32 -0300

    Add Header

--- src/components/activities/Home.js
+++ src/components/activities/Home.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Header from '../Header.js';
+import Logo from '../../images/splash_logo.png';
+import User from '../../images/1.jpg';
+import HeaderItem from '../HeaderItem.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,9 +12,32 @@ export default class Home extends Component {
     super(props);
     this.state = {
     }
-  };
+  }
 
+  componentDidMount() {
+    axios.get('https://hangry-api.herokuapp.com/')
+         .then((data) => {
+            this.props.setStore({
+              data: {
+                data
+              }
+            })
+    });
+  }
   render() {
-    return;
+    return (
+      <div className="activity home">
+        <Header>
+          <div className="activity__header">
+            <HeaderItem>
+            <img className="activity__header-logo" src={Logo} />
+            </HeaderItem>
+            <HeaderItem>
+            <img className="activity__header-user" src={User} />
+            </HeaderItem>
+          </div>
+        </Header>
+      </div>
+    );
   }
 }
