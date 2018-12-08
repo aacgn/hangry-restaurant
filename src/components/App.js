59b488d84705c35d4e8e9d4b9ba03fb0177decb8
aacgn <4augusto@gmail.com>
2018-12-07 21:36:15 -0300

    Kickoff

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { HashRouter, Switch, Route, FadeIn } from 'react-router-dom';
+import HomeActivity from './activities/Home';
+import '../styles/index.scss';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSideMenuVisible: false,
+      userName: 'Burguer King',
+      data: {
+        orders: [],
+      }
+    }
+
+    this.setStore = this.setStore.bind(this);
+  }
+
+  setStore(newState) {
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <>
+        <meta name="theme-color" content="#fe5722"></meta>
+        <HashRouter>
+          <Switch>
+            <Route path="/" render={ 
+              props => <HomeActivity store={this.state} store={this.state} setStore={s => this.setStore(s)} {...props} />} 
+            />
+          </Switch>
+        </HashRouter>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/activities/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  };
+
+  render() {
+    return;
+  }
+}
